168e85fa486f323dc12b1eb72e29ce6e1ce2905d
Santos1012 <santawyomar@gmail.com>
2023-10-24 00:13:24 +0300

    refactor initState & create home then navigate to.

--- lib/features/splash/presentations/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentations/views/widgets/splash_view_body.dart
@@ -1,6 +1,8 @@
-import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentations/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -16,6 +18,22 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
+    initSlidingAnimation();
+    navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlidingText(slidingAnimation: slidingAnimation);
+  }
+
+  void initSlidingAnimation() {
     animationController =
         AnimationController(vsync: this, duration: const Duration(seconds: 3));
     slidingAnimation =
@@ -24,14 +42,16 @@ class _SplashViewBodyState extends State<SplashViewBody>
     animationController.forward();
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-    animationController.dispose();  
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SlidingText(slidingAnimation: slidingAnimation);
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 4),
+      () {
+        Get.to(
+          () => const HomeView(),
+          transition: Transition.circularReveal,
+          duration: kTransitionDuration,
+        );
+      },
+    );
   }
 }
