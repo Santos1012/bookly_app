0d3a927989c2e69d7e56714d44091cbf54084d24
Santos1012 <santawyomar@gmail.com>
2023-11-14 01:11:51 +0200

    bestSellerListViewItem part 1

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_list_view.dart';
@@ -21,7 +22,37 @@ class HomeViewBody extends StatelessWidget {
               style: Styles.titleMedium,
               textAlign: TextAlign.left,
             ),
-          )
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.6 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.test),
+                ),
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              height: 130,
+            ),
+          ),
+       
         ],
       ),
     );
